b8aa016df4ca957fa38548e385f90b3478c64bdf
Antonio Codrea <antonio.codrea@evozon.com>
2020-02-28 15:49:31 +0200

    Finished container

--- config/services.php
+++ config/services.php
@@ -1,12 +1,30 @@
 <?php
 
+use Framework\Contracts\DispatcherInterface;
+use Framework\Contracts\RendererInterface;
+use Framework\Controller\UserController;
+use Framework\Dispatcher\Dispatcher;
+use Framework\Renderer\Renderer;
+use Framework\Router\Router;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+use Framework\Contracts\RouterInterface;
 
+$config = require __DIR__. '/config.php';
 $container = new ContainerBuilder();
-$container->register('Framework\Contracts\RouterInterface', 'Framework\Routing\Router');
-
-
-// .........
-
+$container->setParameter('config', $config);
+$container->register(RouterInterface::class, Router::class)
+            ->addArgument('%config%');
+$container->setParameter('baseViewPath', __DIR__.'/views');
+$container->register(RendererInterface::class, Renderer::class)
+    ->addArgument('%baseViewPath%');
+$container->register(\Framework\Controller\UserController::class,UserController::class)
+    ->addArgument(new Reference(RendererInterface::class));
+$container->setParameter('controllerNamespace', $config['dispatcher']['controllerNamespace']);
+$container->setParameter('controllerSuffix', $config['dispatcher']['controllerSuffix']);
+$container->register(DispatcherInterface::class, Dispatcher::class)
+    ->addArgument('%controllerNamespace%')
+    ->addArgument( '%controllerSuffix%')
+    ->addMethodCall('addController', [new Reference(UserController::class)]);
 
 return new \Framework\DependencyInjection\SymfonyContainer($container);
