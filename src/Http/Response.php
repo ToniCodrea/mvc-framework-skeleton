8daeddb88cd936d33b2cc6a7a8036d1651b4a536
Antonio Codrea <antonio.codrea@evozon.com>
2020-03-06 14:21:42 +0200

    Added location header to response

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -9,6 +9,7 @@ class Response extends Message implements ResponseInterface
 {
     private $statusCode;
     private $reason;
+    private $location;
 
     /**
      * Response constructor.
@@ -16,10 +17,11 @@ class Response extends Message implements ResponseInterface
      * @param string $protocolVersion
      * @param int $statusCode
      */
-    public function __construct(StreamInterface $body, string $protocolVersion = "1.1", $statusCode = 200)
+    public function __construct(StreamInterface $body, string $protocolVersion = "1.1", $statusCode = 200, $location = null)
     {
         parent::__construct($protocolVersion, $body);
         $this->statusCode = $statusCode;
+        $this->location = $location;
 
     }
 
@@ -37,6 +39,9 @@ class Response extends Message implements ResponseInterface
      */
     private function sendHeaders(): void
     {
+        if ($this->location) {
+            header($this->location);
+        }
         if ($this->headers) {
             foreach ($this->headers as $header => $value) {
                 header($header . ":". implode(",", $value));
